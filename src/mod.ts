f12b201124ddf8034dfbe23fc54835ff3e6a9caa
Starscade <angus@starsca.de>
2026-02-09 21:28:50 -0800

    Added JSDoc for default export.

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,8 @@
+/**
+ * @param pgrst_url The PostgREST server URL. (Defaults to "localhost:3000".)
+ * @param schema A comma-separated string of PostgreSQL schemas to connect to. (Defaults to "public".)
+ * @param jwt Specify client JWT to use for authentication. (Defaults to a blank string.)
+ */
 export default class {
 
 	pgrst_url: string
