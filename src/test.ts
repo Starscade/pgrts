5ce19340d68e115552a769019408d264ab8b0a6d
Starscade <angus@starsca.de>
2026-04-09 09:50:52 -0700

    Renamed PGRTS vars to PGRST.

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
 import PostgREST from '../esm/postgrets.esm.js'
 
 const SQL = new PostgREST({
-	pgrst_url: Deno.env.get('PGRTS_HOST'),
-	schema: Deno.env.get('PGRTS_SCHEMA'),
+	pgrst_url: Deno.env.get('PGRST_HOST'),
+	schema: Deno.env.get('PGRST_SCHEMA'),
 })
 
 const result = await SQL.query(Deno.args[0])
